869dd615e044623b6007d635985f7838637029d0
abdoSobhy1 <abdoosobhy97@gmail.com>
2022-11-11 22:47:34 +0200

    sections

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
-let search = document.getElementById("search");
-let label = document.querySelector(".search-label");
-let focusFunc = function () {
-  if (search.value != "" && search.value.trim() != "") {
-    label.classList.add("focused");
-  } else {
-    label.classList.remove("focused");
-  }
-};
-search.addEventListener("focusout", focusFunc);
-
+let input = document.querySelectorAll("#input-fc");
+input.forEach((item) => {
+  item.addEventListener("focusout", function () {
+    if (item.nextElementSibling.classList.contains("label-fc"))
+      label = item.nextElementSibling;
+    if (this.value != "" && this.value.trim() != "") {
+      label.classList.add("focused");
+    } else {
+      label.classList.remove("focused");
+    }
+  });
+});
 document.querySelectorAll(".sidebar li a").forEach((item) => {
   item.addEventListener("click", (event) => {
     document.querySelectorAll(".sidebar li a").forEach((i) => {
@@ -17,3 +18,24 @@ document.querySelectorAll(".sidebar li a").forEach((item) => {
     item.classList.add("active");
   });
 });
+
+let sidebar = document.querySelector(".sidebar");
+let title = function () {
+  if (sidebar.offsetWidth > 58) {
+    document.querySelector(".sidebar > h3").innerHTML = "AbdelRahman";
+  } else {
+    document.querySelector(".sidebar > h3").innerHTML = "ABDO";
+  }
+};
+window.onload = title;
+window.onresize = title;
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".head").style.top = "0px";
+  } else {
+    document.querySelector(".head").style.top = "-90px";
+  }
+  prevScrollpos = currentScrollPos;
+};
