6ce103bcb0663459ead6316d73e94d55e08b04c1
abdoSobhy1 <abdoosobhy97@gmail.com>
2022-11-12 20:47:55 +0200

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -16,6 +16,7 @@ document.querySelectorAll(".sidebar li a").forEach((item) => {
       i.classList.remove("active");
     });
     item.classList.add("active");
+    progress;
   });
 });
 
@@ -47,9 +48,34 @@ let section = document.querySelector(".targets");
 let progress = function () {
   if (window.scrollY >= section.offsetTop - 600) {
     progressSpans.forEach((span) => {
-      span.style.width = span.dataset.prog;
+      if (span.style.width != span.dataset.prog) {
+        span.style.width = span.dataset.prog;
+      }
     });
   }
 };
 
+let lastUrl = location.href;
+new MutationObserver(() => {
+  const url = location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    onUrlChange();
+  }
+}).observe(document, { subtree: true, childList: true });
+
+function onUrlChange() {
+  let link = location.href.toString();
+  if (link.includes("index")) {
+    let progressSpans = document.querySelectorAll(".progress > span");
+    let section = document.querySelector(".targets");
+    if (window.scrollY >= section.offsetTop - 600) {
+      progressSpans.forEach((span) => {
+        if (span.style.width != span.dataset.prog) {
+          span.style.width = span.dataset.prog;
+        }
+      });
+    }
+  }
+}
 window.onload = progress;
